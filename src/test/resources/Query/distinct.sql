b344edb3c6074ba1bf04b03cfe131b6055dd66bb
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-11-15 00:37:13 +0500

    distinct command

---
+++ src/test/resources/Query/distinct.sql
@@ -0,0 +1,23 @@
+select * from EMPLOYEES;
+
+select FIRST_NAME
+from EMPLOYEES;
+
+select distinct FIRST_NAME
+from EMPLOYEES;
+
+select distinct *
+from EMPLOYEES;
+-- it will check each row to filter different columns
+
+select JOB_ID
+from JOBS;
+
+select distinct JOB_ID
+from JOBS;
+
+select distinct JOB_ID
+from EMPLOYEES;
+
+select distinct COUNTRY_ID
+from LOCATIONS;
--- src/test/resources/Query/selectAll.sql
+++ src/test/resources/Query/selectAll.sql
@@ -9,3 +9,10 @@ select FIRST_NAME, LAST_NAME, JOB_ID from EMPLOYEES;
 select * from JOBS;
 
 select * from JOB_HISTORY;
+
+select * from COUNTRIES;
+
+select COUNTRY_NAME, REGION_ID from COUNTRIES;
+
+select JOB_ID, JOB_TITLE
+from JOBS;
