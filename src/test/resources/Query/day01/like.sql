2f663079c54c0585f05cee3cc8a1247995f178cf
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-11-15 02:49:04 +0500

    like keyword

---
+++ src/test/resources/Query/day01/like.sql
@@ -0,0 +1,22 @@
+select *
+from EMPloyees;
+
+select *
+from EMPLOYEES
+where FIRST_NAME LIKE 'B%';
+
+select *
+from EMPLOYEES
+where FIRST_NAME like '__z__';
+
+select *
+from EMPLOYEES
+where FIRST_NAME like '_a%';
+
+select *
+from EMPLOYEES
+where JOB_ID like '%PR%';
+
+select *
+from EMPLOYEES
+where FIRST_NAME like 'H%l';
