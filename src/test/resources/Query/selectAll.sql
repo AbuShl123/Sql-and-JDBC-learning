586ab90114128e9a72d45062d587415e9df6151e
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-11-14 22:52:02 +0500

    some more of coding! SQL

--- src/test/resources/Query/selectAll.sql
+++ src/test/resources/Query/selectAll.sql
@@ -2,4 +2,10 @@ select * from EMPLOYEES;
 
 select EMPLOYEE_ID, FIRST_NAME, PHONE_NUMBER from EMPLOYEES;
 
-select FIRST_NAME from EMPLOYEES
+select FIRST_NAME from EMPLOYEES;
+
+select FIRST_NAME, LAST_NAME, JOB_ID from EMPLOYEES;
+
+select * from JOBS;
+
+select * from JOB_HISTORY;
