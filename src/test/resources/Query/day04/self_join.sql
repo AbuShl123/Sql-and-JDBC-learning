28594c530d9d6b5140ff00ebaaa098d0da0c66ef
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-11-19 02:59:58 +0500

    self-joining
    that one is actually tricky

---
+++ src/test/resources/Query/day04/self_join.sql
@@ -0,0 +1,36 @@
+select * from EMPLOYEES;
+
+-- 7. Display manager of 'Neena'
+select MANAGER_ID
+from EMPLOYEES
+where FIRST_NAME='Neena';
+
+select FIRST_NAME, LAST_NAME
+from EMPLOYEES
+where EMPLOYEE_ID = (select MANAGER_ID
+                     from EMPLOYEES
+                     where FIRST_NAME = 'Ismael');
+
+
+-- Are we gonna find all managers first_name and last_name manually?
+-- no
+-- that's why we need self join
+
+select EMPLOYEE_ID, FIRST_NAME, LAST_NAME, MANAGER_ID
+from EMPLOYEES;
+
+select WORKERS.FIRST_NAME, WORKERS.LAST_NAME, MANAGERS.FIRST_NAME, MANAGERS.LAST_NAME
+from EMPLOYEES WORKERS inner join EMPLOYEES MANAGERS
+ on WORKERS.MANAGER_ID = MANAGERS.EMPLOYEE_ID;
+
+select WORKERS.FIRST_NAME, WORKERS.LAST_NAME, MANAGERS.FIRST_NAME, MANAGERS.LAST_NAME
+from EMPLOYEES WORKERS left join EMPLOYEES MANAGERS
+on WORKERS.MANAGER_ID = MANAGERS.EMPLOYEE_ID;
+
+
+-- count how many workers each manager has?
+select MANAGERS.FIRST_NAME, MANAGERS.LAST_NAME, count(*) as NUMBER_OF_WOKERS
+from EMPLOYEES WORKERS inner join EMPLOYEES MANAGERS
+on WORKERS.MANAGER_ID = MANAGERS.EMPLOYEE_ID
+group by MANAGERS.FIRST_NAME, MANAGERS.LAST_NAME;
+
